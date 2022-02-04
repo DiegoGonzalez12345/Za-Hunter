717ea58c157199ca983ce5d09652faceeec6dfe3
Diego Gonzalez <45858@psdschool.org>
2022-02-04 11:13:18 -0700

    Hi

--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -37,7 +37,42 @@ var currentLocation : CLLocation!
     {
 currentLocation = locations[0]
     }
+    @IBAction func whenSearchButtonPressed(_ sender: Any)
+    
+    {
+    //Hi Briana
+       let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        request.region = MKCoordinateRegion(center: center, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+        guard let response = response else { return }
+        for mapItem in response.mapItems {
+        self.parks.append(mapItem)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = mapItem.placemark.coordinate
+        annotation.title = mapItem.name
+        self.mapView.addAnnotation(annotation)
+        self.mapView.setRegion(region, animated: true)
+        
+        
+    }
+   
+            
+            
+        }
 
+    }
+    @IBAction func whenZoomButtonPressed(_ sender: Any)
+    {
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapView.setRegion(region, animated: true)
+        
+        
+    }
 }
-
-
